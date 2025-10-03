4461e08383c96010262d84f48078a113bddbe2b0
martingm <martin.glumineau@eleve.ensai.fr>
2025-10-03 15:02:38 +0000

    evenement 2.1

--- src/evenement.py
+++ src/evenement.py
@@ -1,15 +1,19 @@
 from datetime import datetime
 
 class Evenement:
-    def __init__(self, id_event, date_event, titre, description_event, lieu, date_evenement, capacite_max, created_by, ):
+    def __init__(self, id_event, date_event, titre, description_event, lieu, capacite_max, created_by, created_at, tarif ):
         self.id_event = id_event
         self.date_event = date_event
         self.titre = titre
         self.description_event = description_event
         self.lieu = lieu
         self.capacite_max = capacite_max
-        
+        self.created_by = created_by
+        self.created_at = created_at if created_at else datetime.now()
+        self.tarif = tarif
         self.participants = []
+        self.bus_aller = None
+        self.bus_retour = None
 
     def inscrire(self, utilisateur):
         if utilisateur not in self.participants:
