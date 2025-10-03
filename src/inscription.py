29144c84fd7d38b88ccf0f6a4acf9d94a2f2da6b
lucasrt <lucas.repetti--tuya@eleve.ensai.fr>
2025-10-03 14:41:16 +0000

    classe inscription

---
+++ src/inscription.py
@@ -0,0 +1,20 @@
+import uuid
+from datetime import datetime
+
+class Inscription:
+    _compteurs = {}
+
+    def __init__(self, boit, mode_paiement, id_event, nom_event, id_bus_aller, id_bus_retour):
+        if nom_event not in Inscription._compteurs:
+            Inscription._compteurs[nom_event] = 0
+        Inscription._compteurs[nom_event] += 1
+        numero = Inscription._compteurs[nom_event]
+        code_unique = uuid.uuid4().hex[:8]
+        self.id = f"{nom_event}_{numero}_{code_unique}"
+        self.boit = boit
+        self.mode_paiement = mode_paiement
+        self.id_event = id_event
+        self.nom_event = nom_event
+        self.id_bus_aller = id_bus_aller
+        self.id_bus_retour = id_bus_retour
+        self.date_creation = datetime.now()
