02964743bbbb81e1da6e422e6df28fdb71170aa6
id2655 <nadia.soumaila-sekou-bery@eleve.ensai.fr>
2025-10-03 14:56:07 +0000

    ajout du dossier business_object

---
+++ src/business_object/utilisateur.py
@@ -0,0 +1,33 @@
+class Utilisateur:
+    " Définit les informations de connection et les caractéristiques d'un utilisateur " 
+
+    def __init__(self, id_utilisateur =  None, pseudo, nom, prenom, email, mdp):
+
+        """ Constructeur de la classe Utilisateur 
+
+        Param
+        -----
+        param id_utilisateur: int ou None - Identifiant auto-généré par la BDD
+        param pseudo : str - Pseudonyme que l'utilisateur se donne (unique, peut servir d'identifiant de connexion)
+        param nom: str - Nom  de l'utilisateur
+        param prénom: str - prénom de l'utilisateur
+        param email: str - Adresse email (unique, peut servir d'identifiant de connexion)
+        param mdp: str - hash du mot de passe
+
+        return : aucun retour
+        -----
+        """
+    
+        self.id_utilisateur = id_utilisateur
+        self.pseudo = pseudo
+        self.nom = nom
+        self.prenom = prenom
+        self.email = email
+        self.mdp = mdp  # Valeur hachée (gérée par AuthService)
+ 
+    
+    def __repr__(self):
+        """
+        Représentation de l'objet de type utilisateur qui sera créé
+        """
+        return f"<Utilisateur id={self.id_utilisateur}, email={self.email}>"
