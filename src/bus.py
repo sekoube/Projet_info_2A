08668389792677680da6a9512b647f164a2b5d87
id2655 <nadia.soumaila-sekou-bery@eleve.ensai.fr>
2025-10-21 08:33:34 +0000

    nadia is in the place

--- src/bus.py
+++ src/bus.py
@@ -1,5 +1,6 @@
 class Bus:
     def __init__(self, id_bus, id_event, sens, description, heure_depart):
+    
         self.id_bus = id_bus
         self.id_event = id_event
         self.sens = sens
