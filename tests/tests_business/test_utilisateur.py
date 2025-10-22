3d838916678027811726c302ae83d7214d5a377b
martingm <martin.glumineau@eleve.ensai.fr>
2025-10-22 14:30:09 +0000

    test again

--- tests/tests_business/test_utilisateur.py
+++ tests/tests_business/test_utilisateur.py
@@ -1,6 +1,11 @@
 import re
 import pytest
 from datetime import datetime
+import sys
+import os
+# Chemin du projet racine (2 niveaux au-dessus du fichier test)
+project_root = os.path.abspath(os.path.join(os.path.dirname(__file__), '../..'))
+sys.path.append(project_root)
 from src.business_object.utilisateur import Utilisateur
 
 
