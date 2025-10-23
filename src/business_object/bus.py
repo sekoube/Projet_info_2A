2c86872f52e8d8c49be6197d0518c8f78b05f32c
ValentinRoseENSAI <valentin.rose@eleve.ensai.fr>
2025-10-23 15:19:04 +0200

    Create bus.py

---
+++ src/business_object/bus.py
@@ -0,0 +1,8 @@
+class Bus:
+    def __init__(self, id_bus, id_event, sens, description, heure_depart):
+    
+        self.id_bus = id_bus
+        self.id_event = id_event
+        self.sens = sens
+        self.description = description
+        self.heure_depart = heure_depart
