b3e8438657b87d3ad5890e531cb34cb1bc761060
valentinrose <valentin.rose@eleve.ensai.fr>
2025-11-06 14:27:47 +0000

    complément accueil_vue

--- src/view/accueil/accueil_vue.py
+++ src/view/accueil/accueil_vue.py
@@ -1,5 +1,6 @@
 from business_object.utilisateur import Utilisateur
 from service.utilisateur_service import UtilisateurService  # Assure-toi que le chemin est correct
+from service.evenement_service import EvenementService
 
 def creer_compte_terminal(service: UtilisateurService):
     print("\n=== Création de compte ===")
@@ -13,6 +14,19 @@ def creer_compte_terminal(service: UtilisateurService):
 
     service.creer_compte(pseudo, nom, prenom, email, mot_de_passe, role)
 
+def creer_event_terminal(service : EvenementService):
+    print("\n=== Création d'un évènement")
+    titre = input("Titre : ")
+    lieu = input("Lieu : ")
+    date_evenement = input("Date : ")
+    capacite_max = input("Capacité max :")
+    created_by = input("Entrez votre identifiant :")
+    description_event = input("Description évènement :")
+    tarif = input("tarif :")
+
+    service.creer_evenement(titre, lieu, date_evenement, capacite_max, created_by, description_event, tarif)
+
+
 def connexion_terminal(service: UtilisateurService):
     print("\n=== Connexion ===")
     email = input("Email : ")
@@ -20,6 +34,24 @@ def connexion_terminal(service: UtilisateurService):
     utilisateur = service.authentifier(email, mot_de_passe)
     if utilisateur:
         print(f"Bienvenue {utilisateur.pseudo} !")
+        print("\n=== Menu de l'utilisateur ===")
+        if UtilisateurService.is_admin():
+            print("1. Créer un évènement")
+            print("2. S'inscrire à un évènement")
+            print("3. Se désinscrire d'un évènement")
+            print("4. Supprimer un évènement")
+            print("5. Supprimer un utilisateur")
+            print("6. Suppremer son compte")
+            print("7. Se déconnecter")
+        else:
+            print("2. S'inscrire à un évènement")
+            print("3. Se désinscrire d'un évènement")
+            print("6. Suppremer son compte")
+            print("7. Se déconnecter")
+        
+        if choix==1:
+            creer_event_terminal(service)
+
     else:
         print("Échec de la connexion.")
 
